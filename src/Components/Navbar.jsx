3196f9154bf0134527731e289660709ef7da9640
muskan2532150 <rg2532150@gmail.com>
2023-03-18 03:10:35 +0530

    Update the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,40 +1,33 @@
-import React from 'react'
-import './navbar.css';
+import React from "react"
+import "./navbar.css";
 
 const Navbar = () => {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary ">
-                <div className="container-fluid">
-                    <a className="navbar-brand ms-2" href="#">L</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">New Project</a>
-                            </li>
+            <nav className="bg-dark position-fixed h-100 text-white">
+                <div className="container-fluid me-auto h-100 fs-4 d-flex flex-column mb-2 align-items-center">
+                    <a className="fs-1" href="#">L</a>
+                    <ul className=" mb-lg-0 flex-grow-1">
+                        <li className="nav-item mb-2">
+                            <a className="nav-link" aria-current="page" href="#">Dashboard</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a className="nav-link" href="#">New Project</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                          <a href="#" className="nav-link" >Courses</a>
+                        </li>
+                    </ul>
+                    <div className="dropdown">
+                        <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown button
+                        </button>
+                        <ul className="dropdown-menu mb-3">
+                            <li><a className="dropdown-item" href="#">Sign Out</a></li>
+                            <li className="dropdown-item">Setting</li>
                         </ul>
-                        <form className="d-flex w-50" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                        <div className="dropdown">
-                            <button className="btn btn-secondary dropdown-toggle rounded ms-1 signout-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Icon
-                            </button>
-                            <ul className="dropdown-menu dropdown-menu-dark">
-                                <li><a className="dropdown-item" href="#">Sign Out</a></li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
-            </nav>
-        </div>
+            </nav >
     )
 }
 
